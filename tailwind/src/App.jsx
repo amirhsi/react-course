06478cb0a1903ae4830688a2f794f84defe642ab
a.hosseini <amirhsii98@gmail.com>
2024-04-18 11:40:16 +0330

    tailwind added

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-green-500 p-3">Hi</h1>
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind/src/components/Card.jsx
@@ -0,0 +1,31 @@
+function Card() {
+  return (
+    <div>
+      <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 rounded-full mx-auto"
+          src="https://tailwindcss.com/_next/static/media/sarah-dayan.de9b3815.jpg"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 text-center space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that large teams. It’s easy to
+              customize, adapts to any design, and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
+            <div className="text-slate-700 dark:text-slate-500">
+              Staff Engineer, Algolia
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+    </div>
+  );
+}
+
+export default Card;
